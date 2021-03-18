a675c2489712d4a2fb474dd29a01360748be9b4d
matheusramires <80134011+matheusramires@users.noreply.github.com>
2021-03-18 18:31:22 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import smtplib
+
+try:
+    while 1:
+        fromaddr = 'seu_email@gmail.com'
+        toaddrs  = 'email_do_destinatario@yahoo.com'
+        msg = 'Spam email Test'
+
+        username = 'seu_email@gmail.com'
+        password = 'sua_senha'
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
+        print('email enviado')
+except:
+    print('falha ao enviar email')
